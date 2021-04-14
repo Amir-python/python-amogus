d631c792213461a9dda9fc846fa067b47d63076e
Amir-python <82511775+Amir-python@users.noreply.github.com>
2021-04-14 17:11:57 +0600

    Add files via upload
    just import it

---
+++ lol.py
@@ -0,0 +1,16 @@
+from turtle import*   
+def rectangle(w, h):
+    for i in range(2):
+        fd(w)
+        left(90)
+        fd(h)
+        left(90)
+
+def square(a):
+    for i in range(4):
+        fd(a)
+        left(90)
+
+def amogus(name):
+    print(name + " импостер")
+    
